325c6128e7bb823daaad96ed7bf9e06276d25357
Jorge G. Moreno <96766903+jkokecas1@users.noreply.github.com>
2024-11-03 17:03:42 -0600

    styles: remove whitespace and add dinamic banner

--- adida/views.py
+++ adida/views.py
@@ -4,14 +4,21 @@ from store.models import Product, ReviewRating
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     banners = [
-        'images/banners/2.jpg',
-        'images/banners/3.jpg',
-        'images/banners/4.jpg',
-        'images/banners/5.jpg',
-        'images/banners/6.jpg',
-        'images/banners/7.jpg',
-        'images/banners/8.jpg',
-        'images/banners/9.jpg'
+        { 'url':'static/images/banners/2.jpg', 'alt': 'First slide', 'class': 'carousel-item active'},
+        { 'url':'static/images/banners/3.jpg', 'alt': 'second slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/4.jpg', 'alt': 'third slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/5.jpg', 'alt': 'fourth slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/6.jpg', 'alt': 'fifth slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/7.jpg', 'alt': 'sixth slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/8.jpg', 'alt': 'seventh slide', 'class': 'carousel-item' },
+        { 'url':'static/images/banners/9.jpg', 'alt': 'eighth slide', 'class': 'carousel-item' },
+    ]
+
+    videoBaner = [
+        'https://brand.assets.adidas.com/video/upload/q_auto,vc_auto,c_scale,w_0.5/video/upload/kids-fw21-disney-lionking-launch-clp-tc-carousel_egivfj.mp4',
+        'https://brand.assets.adidas.com/video/upload/q_auto,vc_auto,c_scale,w_0.5/video/upload/tc_carrousel_hp_mx_adizero_jnqgyv.mp4',
+        'https://brand.assets.adidas.com/image/upload/q_auto,vc_auto,c_scale,w_0.5/esMX/Images/t_carrousel_green_mondays_12_tcm217-831424.mp4',
+        'https://brand.assets.adidas.com/video/upload/q_auto,vc_auto,c_scale,w_0.5/video/upload/global%20brand%20publishing/Statement/ss22-ivp-5/ss22-ivp-5-launch-tcar-m.mp4'
     ]
     # Obtener review
     for product in products:
@@ -19,7 +26,8 @@ def home(request):
     context = {
         'products': products,
         'reviews' : reviews,
-        'banners' : banners
+        'banners' : banners,
+        'videos' : videoBaner
     }
     return render(request, 'home.html', context)
     #return HttpResponse('Homepage')
